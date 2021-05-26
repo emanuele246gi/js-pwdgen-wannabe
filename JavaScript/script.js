aa63bc1f4fbb049063be23abfa7376281e005f84
Emanuele <emanuele65b@gmail.com>
2021-05-26 15:49:52 +0200

    add test 2

---
+++ JavaScript/script.js
@@ -0,0 +1,4 @@
+console.log('funziona? Ma guarda, riesco ad abbattere i muri ma non riesco a mettere questo');
+document.writeln('Hello World!');
+alert('Hello World!');
+document.getElementById('testo').innerHTML = 'Hello World!'
