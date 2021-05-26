ac043590431d7c2cb816f9334a14f97a0b6725e0
Emanuele <emanuele65b@gmail.com>
2021-05-26 17:12:57 +0200

    add correzioni

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+var nomeutente = prompt ('Nome');
+console.log(nomeutente);
+
+var cognomeutente = prompt ('Cognome');
+console.log(cognomeutente);
+
+var coloreutente = prompt ('Colore preferito');
+console.log(coloreutente);
+
+var password = nomeutente + cognomeutente + coloreutente;
+
+document.writeln(password + '21');
