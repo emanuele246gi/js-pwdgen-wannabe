a09aab44eec4026e4b33fd1f991711c325173020
Emanuele <emanuele65b@gmail.com>
2021-05-26 16:04:24 +0200

    add inizio prompt

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,2 @@
-console.log('funziona? Ma guarda, riesco ad abbattere i muri ma non riesco a mettere questo');
-document.writeln('Hello World!');
-alert('Hello World!');
-document.getElementById('testo').innerHTML = 'Hello World!'
+
+var nomeutente = prompt ('Nome')
