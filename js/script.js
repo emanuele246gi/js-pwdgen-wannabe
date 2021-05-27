53f1b8b07b78083e3b17e4b78c8c0fd213fa4b90
Emanuele <emanuele65b@gmail.com>
2021-05-27 15:52:51 +0200

    finishing project (for real)

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 
+// nome
 var nomeutente = prompt ('Nome');
 console.log(nomeutente);
 
+// cognome
 var cognomeutente = prompt ('Cognome');
 console.log(cognomeutente);
 
+// colore
 var coloreutente = prompt ('Colore preferito');
 console.log(coloreutente);
 
+// operazione password
 var password = nomeutente + cognomeutente + coloreutente;
 
+// risultato password
 document.getElementById(password + '21');
